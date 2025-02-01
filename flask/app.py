a8f2a6a1cfcadd0848402966eefe74e45ece322c
Nicholas Mason-Apps <nicholas.mason-apps@live.co.uk>
2025-02-01 16:09:19 +0000

    Added DB pinging

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,10 @@ from pymongo import MongoClient
 app = Flask(__name__)
 client = MongoClient(host="mongodb://ichack25mongo.containers.uwcs.co.uk/", port=27017, username='admin', password='password', authSource="admin")
 
+@app.route("/dbtest")
+def DBTest():
+    return client.admin.command('ping')
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
