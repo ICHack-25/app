88e964327c8073e7794ea804315d47eacc1c9beb
Nicholas Mason-Apps <nicholas.mason-apps@live.co.uk>
2025-02-01 15:58:53 +0000

    DB client

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client = MongoClient(host="mongodb://ichack25mongo.containers.uwcs.co.uk/", port=27017, username='admin', password='password', authSource="admin")
 
 @app.route('/')
 def hello_world():  # put application's code here
